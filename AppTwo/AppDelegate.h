58f84851a743613dbbb499dc0a0f3a8b6752da51
juanelorocha <contact@juanrocha.com>
2014-10-08 22:07:56 -0700

    Initial Commit

---
+++ AppTwo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppTwo
+//
+//  Created by juan rocha on 10/8/14.
+//  Copyright (c) 2014 JuanRocha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppTwo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AppTwo
+//
+//  Created by juan rocha on 10/8/14.
+//  Copyright (c) 2014 JuanRocha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
